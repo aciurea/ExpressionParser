5866e3250cd01c27cff9876f5f2aed20f7435fed
SPARKWARE\alinci <alinciurea90@gmail.com>
2017-02-13 11:20:23 +0200

    removed unnacessary code

--- ExpParser/ExpParser/gruntfile.js
+++ ExpParser/ExpParser/gruntfile.js
@@ -2,22 +2,6 @@
 module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON("package.json"),
-        babel: {
-            options: {
-                sourceMap: false,
-                presets: ["es2015"],
-                plugins: ["transform-es2015-modules-amd"]
-            },
-            dist: {
-                files: [
-                {
-                    expand: true,
-                    cwd: "Scripts/app/src/",
-                    src: ["employee.js", "expressionParser.js", "segmentationBuilder.js", "employeeModule.js"],
-                    dest: "Scripts/app/dist/"
-                }]
-            }
-        },
         browserify: {
             dist: {
                 options: {
@@ -32,29 +16,12 @@ module.exports = function (grunt) {
         uglify: {
             my_target: {
                 files: {
-                    "Scripts/app/expParser.min.js": "Scripts/app/built.js"
+                    "Scripts/app/dist/app.min.js": "Scripts/app/dist/app.js"
                 }
-            }
-        },
-        transpile: {
-            main: {
-                type: "cjs", // or "amd" or "yui"
-                files: [{
-                    expand: true,
-                    cwd: 'Scripts/app/src/',
-                    src: ['*.js'],
-                    dest: 'Scripts/app/tmp/'
-                }]
             }
         }
     });
-    //grunt.loadNpmTasks("grunt-babel");
-    //grunt.loadNpmTasks('grunt-es6-module-transpiler');
-    //grunt.loadNpmTasks("grunt-contrib-uglify");
     grunt.loadNpmTasks("grunt-browserify");
-
-    grunt.registerTask("default", ["browserify"]);
-    //grunt.registerTask("default", ["concat", "babel", "browserify"]);
-    //grunt.registerTask("babelJs", ["babel"]);
-    //grunt.registerTask("browserifyJS", ["browserify"]);
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.registerTask("default", ["browserify", "uglify"]);
 }
